2411844f9656a8f53b948b0692f87bf9b39d0424
bodddos-create <b.odddos@gmail.com>
2026-09-16 11:41:50 +0200

    Ajoute layout application

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'Maison Oddos · Pilotage',description:'Pilotage des restaurants'}
+export default function RootLayout({children}){return <html lang="fr"><body>{children}</body></html>}
