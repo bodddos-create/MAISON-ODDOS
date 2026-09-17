6bdb124887afcd193fe6f4a5942ca9c08b8e1a91
bodddos-create <b.odddos@gmail.com>
2026-09-17 22:17:59 +0200

    Use full approved Direction finance chart

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,6 @@ import TTCDisplayEnhancer from './TTCDisplayEnhancer'
 import PersonnelDailyCost from './PersonnelDailyCost'
 import PersonnelManager from './PersonnelManager'
 import VatFloatingButton from './VatFloatingButton'
-import PersonnelChartRed from './PersonnelChartRed'
+import DirectionFinanceChart from './DirectionFinanceChart'
 export const metadata={title:'Maison Oddos · Pilotage',description:'Pilotage des restaurants'}
-export default function RootLayout({children}){return <html lang="fr"><body><InvoiceTabRouter/><TTCDisplayEnhancer/><PersonnelDailyCost/><PersonnelManager/><PersonnelChartRed/>{children}<VatFloatingButton/></body></html>}
+export default function RootLayout({children}){return <html lang="fr"><body><InvoiceTabRouter/><TTCDisplayEnhancer/><PersonnelDailyCost/><PersonnelManager/><DirectionFinanceChart/>{children}<VatFloatingButton/></body></html>}
