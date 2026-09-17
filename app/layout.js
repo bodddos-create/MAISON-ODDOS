a1d18ccae96ee63e0436ad19dad7827f5219320c
bodddos-create <b.odddos@gmail.com>
2026-09-17 22:13:41 +0200

    Enable red personnel curve in Direction chart

--- app/layout.js
+++ app/layout.js
@@ -4,5 +4,6 @@ import TTCDisplayEnhancer from './TTCDisplayEnhancer'
 import PersonnelDailyCost from './PersonnelDailyCost'
 import PersonnelManager from './PersonnelManager'
 import VatFloatingButton from './VatFloatingButton'
+import PersonnelChartRed from './PersonnelChartRed'
 export const metadata={title:'Maison Oddos · Pilotage',description:'Pilotage des restaurants'}
-export default function RootLayout({children}){return <html lang="fr"><body><InvoiceTabRouter/><TTCDisplayEnhancer/><PersonnelDailyCost/><PersonnelManager/>{children}<VatFloatingButton/></body></html>}
+export default function RootLayout({children}){return <html lang="fr"><body><InvoiceTabRouter/><TTCDisplayEnhancer/><PersonnelDailyCost/><PersonnelManager/><PersonnelChartRed/>{children}<VatFloatingButton/></body></html>}
