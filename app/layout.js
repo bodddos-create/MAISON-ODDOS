328dae8ad5cd332156a31205a43fdf3a63ed443e
bodddos-create <b.odddos@gmail.com>
2026-09-18 10:48:26 +0200

    Intégrer calendrier ouverture

--- app/layout.js
+++ app/layout.js
@@ -6,5 +6,6 @@ import PersonnelManager from './PersonnelManager'
 import VatFloatingButton from './VatFloatingButton'
 import DirectionFinanceChart from './DirectionFinanceChart'
 import DirectionVatSummary from './DirectionVatSummary'
+import OpeningCalendar from './OpeningCalendar'
 export const metadata={title:'Maison Oddos · Pilotage',description:'Pilotage des restaurants'}
-export default function RootLayout({children}){return <html lang="fr"><body><InvoiceTabRouter/><TTCDisplayEnhancer/><PersonnelDailyCost/><PersonnelManager/><DirectionFinanceChart/><DirectionVatSummary/>{children}<VatFloatingButton/></body></html>}
+export default function RootLayout({children}){return <html lang="fr"><body><InvoiceTabRouter/><TTCDisplayEnhancer/><PersonnelDailyCost/><PersonnelManager/><DirectionFinanceChart/><DirectionVatSummary/><OpeningCalendar/>{children}<VatFloatingButton/></body></html>}
